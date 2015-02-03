a4266d6061acfbe8592953fb39b9e3e10dd06536
Mahesh Shanbhag <ShanbhagMahesh@Maheshs-MacBook-Pro.local>
2015-02-03 10:42:12 +0530

    Initial commit with intall.sh and resymbolicating script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+DOWNLOAD_LOCATION="https://github.com/MaheshRS/Kalapani/raw/master/Scripts/resymbolicate"
+USERNAME=$(whoami)
+INSTALL_DIR="/Users/$USERNAME/Humba/Scripts"
+
+rm -rf $INSTALL_DIR
+mkdir -p $INSTALL_DIR
+	
+if grep -q "$INSTALL_DIR" ~/.bash_profile; then
+	# Path already available
+	echo "Installing ..." >&2
+else
+	# Path not available. Add the installation directory path $PATH env variable 
+	echo "Installing ..." >&2
+	echo export PATH=$PATH:$INSTALL_DIR >> ~/.bash_profile
+	source ~/.bash_profile
+fi
+
+cd $INSTALL_DIR
+curl -LOk $DOWNLOAD_LOCATION
+chmod +x "$INSTALL_DIR/resymbolicate"
